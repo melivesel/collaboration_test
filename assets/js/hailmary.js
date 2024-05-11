e58ad00763cc92517d9371128c1c6b9583cbc795
Melissa Bentley <melivesel@gmail.com>
2024-05-11 12:12:45 -0500

    fixed data pathway

--- assets/js/hailmary.js
+++ assets/js/hailmary.js
@@ -4,7 +4,7 @@ fetch("https://gateway.marvel.com:443/v1/public/characters?name=Iron%20Man&apike
     })
     .then(function(data) {
         console.log(data);
-        const characterid = data.results[0].id;
+        const characterid = data.data.results[0].id;
         fetch('https://gateway.marvel.com:443/v1/public/characters/' + characterid + '/comics?limit=25&apikey=f99ff0ebd9b29727ddc4d22f632170a4')
             .then(function(response) {
                 return response.json();
