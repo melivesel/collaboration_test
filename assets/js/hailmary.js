b336daf49f04760a9fc273ecb3a5f62a2c309768
Melissa Bentley <melivesel@gmail.com>
2024-05-11 12:05:27 -0500

    testing pulling from api twice

--- assets/js/hailmary.js
+++ assets/js/hailmary.js
@@ -4,6 +4,17 @@ fetch("https://gateway.marvel.com:443/v1/public/characters?name=Iron%20Man&apike
     })
     .then(function(data) {
         console.log(data);
+        const characterid = data.results[0].id;
+        fetch('https://gateway.marvel.com:443/v1/public/characters/' + characterid + '/comics?limit=25&apikey=f99ff0ebd9b29727ddc4d22f632170a4')
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                console.log(data);
+            })
+            .catch(function(error) {
+                console.log("Error fetching data:", error);
+            });
     })
     .catch(function(error) {
         console.log("Error fetching data:", error);
